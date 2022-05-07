ce9957b9d2251c62c29ebcd41cc4088f88ab9050
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-05-07 17:19:11 -0400

    Initial changes for file uploader functionality

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { signInWithGoogle } from './auth/Firebase';
-
+import FileUploaderModal from "./components/file-uploader-modal/FileUploaderModal.jsx";
 
 class App extends React.Component {
   render() {
@@ -13,13 +13,13 @@ class App extends React.Component {
         <h1>
           Hello {name}
         </h1>
-
-        <button type="button" className="btn btn-primary" onClick={signInWithGoogle}>
+        <FileUploaderModal />
+        {/* <button type="button" className="btn btn-primary" onClick={signInWithGoogle}>
           This is a bootstrap button
-        </button>
-        <h2>{localStorage.getItem("name")}</h2>
+        </button> */}
+        {/* <h2>{localStorage.getItem("name")}</h2>
         <h3>{localStorage.getItem("email")}</h3>
-        <h3>{localStorage.getItem("photoURL")}</h3>
+        <h3>{localStorage.getItem("photoURL")}</h3> */}
       </>
     );
   }
