121d23d1d363b0bf6e7f1360e537bd9f1d816395
Will Atwood <X1@galaxia.us>
2022-05-07 14:19:22 -0400

    Creating the server routes and controllers basic skeleton. (#2)
    * Initial
    
    * Installing server routes and controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,35 @@
-//each functionality will have its own controller
 
-//exports.[name of function] = require(path of file)
-//exports.[name of function] = require(path of file)
 
+// eslint-disable-next-line no-undef
+module.exports = {
+  testFunction: (req, res) => {
+    res.send(200)
+  },
+  getUserData: (req, res) => {
+    console.log('getUserData', req.params)
+    res.send(200)
+  },
+  getSpeeches: (req, res) => {
+    //array of speeches... either grab the first speech if not defined
+    // or grab the speech at index value of request
+    console.log('getSpeeches', req.params)
+    res.send(req.params)
+  },
+  getAnalyzedSpeeches: (req, res) => {
+    console.log('getAnalyzedSpeeches', req.params)
+    res.send(req.params)
+  },
+  postSpeech: (req, res) => {
+    //insert a new speech into database if speech ID doesn't exist, if ID found
+    // unshift() speech into array of user speeches.
+    console.log('postSpeech', req.body)
+    res.send(req.body)
+  },
+  postVideo: (req, res) => {
+    //look up user and add vidoe URL to database
+    //Front End will also post to Firebase storage?
+    console.log('postSpeech', req.body)
+    res.send(req.body)
+  }
+
+}
--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+const router = require('./routes')
 const express = require('express')
 require('dotenv').config()
 const path = require('path')
@@ -8,15 +10,14 @@ const app=express();
 //middleware files
 app.use(express.static(path.join(__dirname, '../dist/src')));
 app.use(express.static('dist'));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(morgan('dev'));
 
+app.use('/', router)
 
+const PORT = process.env.PORT || 3000
 
-app.get('/test', (req, res)=> {
-  res.send("<h1>Hello World</h1>")
-})
-
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
