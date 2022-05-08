8d8ad5dda7f651b3ca1a54df1bb3c7ae673d3303
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-05-08 19:15:22 -0400

    Implementing Drag and Drop feature

--- src/components/file-uploader-modal/FileUploaderModal.jsx
+++ src/components/file-uploader-modal/FileUploaderModal.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import axios from 'axios';
+import { useDropzone } from 'react-dropzone'
+import { BiLeftArrowAlt } from 'react-icons/bi'
 import './FileUploaderModal.css';
 
-function FileUploaderModal() {
+function FileUploaderModal(props) {
+  const [fileName, setFileName] = useState('');
+  const [files, setFiles] = useState([]);
+
+  const onDrop = acceptedFiles => {
+    // Do something with the files
+  }
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  const uploadFile = (e) => {
+    e.preventDefault();
+    // axios.get('/endpoint', {data})
+  }
+
   return (
-    <div className="file-uploader-modal">
-      <div className="file-uploader-back-button">
-        <a href="#"> Back</a>
-      </div>
-      <div className="file-uploader-modal-content">
-        <span className="file-uploader">FILE UPLOADER</span>
-        <button className="submit-speech-button">SUBMIT</button>
-      </div>
-      <div className="results-area">
-        <span className="word-analyzed">WORD</span>
+    <div className={`file-uploader-modal ${props.show ? 'file-uploader-modal-show' : ''}`} onClick={props.onClose}>
+      <div className="file-uploader-modal-content" onClick={e => e.stopPropagation()}>
+
+        <div className="file-uploader-back-button">
+          <BiLeftArrowAlt onClick={props.onClose} />
+        </div>
+        <div className="file-uploader-area">
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {
+              isDragActive ?
+                <p className="file-uploader">Drop the files here ...</p> :
+                <p className="file-uploader">Drag and drop some files here</p>
+            }
+            <p>{fileName}</p>
+            <button className="submit-speech-button" onClick={uploadFile}>Submit</button>
+          </div>
+        </div>
+        <div className="results-area">
+          <span className="word-analyzed">WORD</span>
+        </div>
       </div>
     </div>
   )
