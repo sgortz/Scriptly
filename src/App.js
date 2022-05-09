c2f6c1f7d31e9733706548033ba55293fe810ce5
Jonathan Ghee <jonathan.p.ghee@gmail.com>
2022-05-09 09:50:39 -0500

    added signup component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { signInWithGoogle } from './auth/Firebase';
 import Header from './components/Header.jsx'
 import Landing from './components/Landing/Landing.jsx'
+import SignUp from './components/SignUp'
 
 function App () {
   const [page, setPage] = useState('landing')
-  const currentPage = page === 'landing' ? <Landing /> : page === 'signin' ? <Signin /> : <Homepage />
+  const [login, setLogin] = useState(false)
 
+  const currentPage = page === 'landing' ? <Landing /> : page === 'signin' ? <Signin /> : <Homepage />
     return (
       <div className="app-container">
       <Header />
+      <a onClick={() => setLogin(true)}>Log-in</a>
+      <SignUp/>
+      {login && (<dialog><SignUp /></dialog>)}
       {currentPage}
       </div>
     );
