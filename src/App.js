594103680613a5ee4b2babb554ff95c66cd75da8
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-05-09 10:57:44 -0400

    Homepage 01 (#7)
    * Initial Commit - Homepage Framework Coming Together
    
    * Working on Text Editor Functionality - Setting to Global States
    
    * Finding Limitation With Built In Text Area Property
    
    * Reverted App.js
    
    * Conflicts
    
    * Conflicts
    
    * Fixed Tabs
    
    * Working On Editable TextArea
    
    * Added Homepage to Landing Page
    
    Co-authored-by: Trevor Edwards <“trevor.jeffrey.edwards@gmail.com”>

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import React, { useState } from "react";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { signInWithGoogle } from './auth/Firebase';
-import Header from './components/Header.jsx'
-import Landing from './components/Landing/Landing.jsx'
+import Header from './components/Header.jsx';
+import Landing from './components/Landing/Landing.jsx';
+import Homepage from './components/Homepage/homepage.jsx';
 
 function App () {
   const [page, setPage] = useState('landing')
   const currentPage = page === 'landing' ? <Landing /> : page === 'signin' ? <Signin /> : <Homepage />
 
     return (
+      <>
       <div className="app-container">
-      <Header />
-      {currentPage}
-      </div>
+        <Header />
+        {currentPage}
+        <Homepage/>
+        {/* <h1>
+          Hello {name}
+        </h1>
+
+        <button type="button" className="btn btn-primary" onClick={signInWithGoogle}>
+          This is a bootstrap button
+        </button>
+        <h2>{localStorage.getItem("name")}</h2>
+        <h3>{localStorage.getItem("email")}</h3>
+        <h3>{localStorage.getItem("photoURL")}</h3> */}
+        </div>
+      </>
     );
 }
 
