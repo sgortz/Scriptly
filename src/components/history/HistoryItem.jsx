3fcca09cb0cf4ec7c8fcd51bf55fb3ef22f45ae7
Peter Schaefferkoetter <prschaefferkoetter@gmail.com>
2022-05-10 14:20:49 -0600

    History click (#36)
    * resolve merge conflict on local
    
    * resolve merge conflicts on local
    
    * resolve merge conflict on local
    
    * modfify async functionality
    
    * update history item functionality
    
    * update history data flow

--- src/components/history/HistoryItem.jsx
+++ src/components/history/HistoryItem.jsx
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import DoughnutChart from '../charts/DoughnutChart.jsx';
+import {resultsModal} from '../../atoms.jsx';
+import {useRecoilState} from 'recoil';
+import Results from '../../results/Results.jsx';
 
 function HistoryItem(props) {
-  const { date, body, analysis } = props;
+  const { date, body, analysis, title } = props;
+  const [showResults, setShowResults] = useRecoilState(resultsModal);
   let bodyPreview = body.slice(0, 150);
 
+  console.log('inside result', showResults);
+
+  const sendResults = (e) => {
+     setShowResults(true);
+     console.log('insideitem',e);
+  }
+
   return (
-    <li className="speech-history-list-item">
+    <li className="speech-history-list-item" onClick={(e)=> { sendResults(e) }}>
       <small className="speech-date">{date}</small>
       <div className="speech-preview-chart">
         <p className="speech-preview-body">{`${bodyPreview}...`}</p>
