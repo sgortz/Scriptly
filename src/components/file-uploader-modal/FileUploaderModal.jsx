67c9841b381590e3a484331d23a3a23396fad539
pschaefferkoetter <prschaefferkoetter@gmail.com>
2022-05-10 10:14:05 -0600

    add text parse functionality

--- src/components/file-uploader-modal/FileUploaderModal.jsx
+++ src/components/file-uploader-modal/FileUploaderModal.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useDropzone } from 'react-dropzone'
 import { BiLeftArrowAlt } from 'react-icons/bi'
 import './FileUploaderModal.css';
+const lib = require('../shared/ScriptlyShared.js');
 
 function FileUploaderModal(props) {
   const [fileName, setFileName] = useState('');
@@ -29,7 +30,14 @@ function FileUploaderModal(props) {
 
   const uploadFile = (e) => {
     e.preventDefault();
-    // axios.get('/speech', {data})
+    let words = lib.parseTextToArray(files[0]);
+    let totalWordCount = words.length;
+    // analysis here
+
+    // need users email, speech title,
+   // axios.post(/speech)
+
+
     alert('thank you for submitting')
   }
 
@@ -50,6 +58,7 @@ function FileUploaderModal(props) {
             }
             <p>{fileName}</p>
           </div>
+            <input type="text" name="title" placeholder="Enter speech title"/>
             <button className="submit-speech-button" onClick={uploadFile}>Submit</button>
           <ul>{acceptedFileItems}</ul>
         </div>
