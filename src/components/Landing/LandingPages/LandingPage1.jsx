93219c73e8f37a401df3361fa469dd4a77a68dc8
akb3y <alisha.barraw@gmail.com>
2022-05-11 18:31:46 -0500

    landing carousel

---
+++ src/components/Landing/LandingPages/LandingPage1.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function LandingPage2() {
+
+    return (
+      <div className="headerText">
+        <br /><br /><br /><br />
+             <div className="headerTextLarger">Get the tone of any speech <br />
+            you create. Instantly. <br /><br /> </div>
+            <div className="headerTextSmall">Over 220,000 speeches use Scriptly to
+             <br />
+            improve what’s working and fix what isn’t. </div><br /> <br />
+      </div>
+    );
+}
+
+export default LandingPage2;
---
+++ src/components/Landing/LandingPages/LandingPage2.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function LandingPage2() {
+
+    return (
+        <div className="carouselOne">
+      <div className="carousel">
+                I have a dream today! I have a dream that one day <br />
+                every valley shall be exalted, and every hill and<br />
+                mountain shall be made low, the rough places will<br />
+                be made plain, and the crooked places will be<br />
+                made straight; "and the glory of the Lord shall be<br />
+                revealed and all flesh shall see it together."<br />
+                -MLK, 1963
+            </div>
+            <img src='https://firebasestorage.googleapis.com/v0/b/scriptly-dcd67.appspot.com/o/Screen%20Shot%202022-05-11%20at%205.04.05%20PM.png?alt=media&token=bed42cda-50ed-4a41-b63e-09902847c1bf' alt="I have a dream" />
+        </div>
+
+
+    );
+}
+
+export default LandingPage2;
