5dcf1af0b189ff019d1b5ca3d87bd9d147f3daf1
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-05-11 12:18:33 -0400

    Update to remove unnecessary states that got replaced by recoil

--- src/components/file-uploader-modal/fileUploaderModal.jsx
+++ src/components/file-uploader-modal/fileUploaderModal.jsx
@@ -1,26 +1,22 @@
 import React, { useState, useCallback } from 'react';
 import { useRecoilState } from 'recoil';
-import { editedSpeechText, resultsModal } from '../../atoms.jsx';
-import { useDropzone } from 'react-dropzone'
-import {useRecoilState} from 'recoil';
-import {editedSpeechText} from '../../atoms.jsx';
-import { BiLeftArrowAlt } from 'react-icons/bi'
+import { BiLeftArrowAlt } from 'react-icons/bi';
+import { useDropzone } from 'react-dropzone';
+import { editedSpeechText, resultsModal, updateTitle } from '../../atoms.jsx';
 import './FileUploaderModal.css';
 
 function FileUploaderModal(props) {
-
   const [fileName, setFileName] = useState('');
   const [files, setFiles] = useState(null);
   const [enableButton, setEnableButton] = useState(true);
   const [editedValue, setEdited] = useRecoilState(editedSpeechText);
   const [showResults, setShowResults] = useRecoilState(resultsModal);
-  const [page, setPage] = useState(1);
   const [titleValue, setTitle] = useRecoilState(updateTitle);
-  
-  const onDrop = useCallback(acceptedFiles => {
-    acceptedFiles.map(file => {
+
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.map((file) => {
       const reader = new FileReader();
-      reader.onload = function (e) {
+      reader.onload = (e) => {
         setFiles([e.target.result]);
       };
       reader.readAsText(file);
@@ -28,27 +24,40 @@ function FileUploaderModal(props) {
     });
   }, []);
 
-  const { getRootProps, getInputProps, acceptedFiles, isDragActive } = useDropzone({ onDrop });
+  const {
+    getRootProps, getInputProps, acceptedFiles, isDragActive,
+  } = useDropzone({ onDrop });
 
-  const acceptedFileItems = acceptedFiles.map(file => (
+  const acceptedFileItems = acceptedFiles.map((file) => (
     <li className="uploaded-file-info" key={file.path}>
-      <p><strong>Title: </strong>{speechTitle}</p>
-      <p><strong>Filename: </strong>{file.path}</p>
-      <p><strong>File size: </strong>{file.size} bytes</p>
+      <p>
+        <strong>Title: </strong>
+        {titleValue}
+      </p>
+      <p>
+        <strong>Filename: </strong>
+        {file.path}
+      </p>
+      <p>
+        <strong>File size: </strong>
+        {file.size}
+        bytes
+      </p>
     </li>
   ));
 
   const uploadFile = (e) => {
+    console.log(files[0]);
     e.preventDefault();
     // sending to speech analysis
-    setEdited(files[0])
-    // send the info to results' modal 
+    setEdited(files[0]);
+    // send the info to results' modal
     setShowResults(true);
-  }
-    
-    return (
-      <div className={`file-uploader-modal ${props.show ? 'file-uploader-modal-show' : ''}`} onClick={props.onClose}>
-      <div className="file-uploader-modal-content" onClick={e => e.stopPropagation()}>
+  };
+
+  return (
+    <div className={`file-uploader-modal ${props.show ? 'file-uploader-modal-show' : ''}`} onClick={props.onClose} aria-hidden="true">
+      <div className="file-uploader-modal-content" role="button" onClick={e => e.stopPropagation()} >
         <div className="file-uploader-back-button">
           <BiLeftArrowAlt onClick={props.onClose} />
         </div>
@@ -56,29 +65,33 @@ function FileUploaderModal(props) {
           <div {...getRootProps()}>
             <input {...getInputProps()} />
             {
-              isDragActive ?
-              <p className="file-uploader">Drop the files here</p> :
-                <p className="file-uploader">Drag and drop or click here to add some files </p>
+              isDragActive
+                ? <p className="file-uploader">Drop the files here</p>
+                : <p className="file-uploader">Drag and drop or click here to add some files </p>
             }
-            <p>{fileName}</p>
+            {/* Not sure if the line below is doing anything */}
+            {/* <p>{fileName}</p> */}
           </div>
-          <input type="text"
+          <input
+            type="text"
             className="form-control speech-title-input"
             name="title"
             placeholder="Enter speech title"
-            value={speechTitle}
+            value={titleValue}
             onChange={
-              e => {
+              (e) => {
                 setTitle(e.target.value);
                 setEnableButton(false);
-              }} />
-          {speechTitle.length === 0 ? <div className="form-text">Please add a title to your speech before submitting.</div> : null}
+              }
+            }
+          />
+          {titleValue.length === 0 ? <div className="form-text">Please add a title to your speech before submitting.</div> : null}
           <ul style={{ listStyle: 'none', marginTop: '2em', padding: 0 }}>{acceptedFileItems}</ul>
-          <button className="submit-speech-button" onClick={uploadFile} disabled={enableButton}>Submit</button>
+          <button className="submit-speech-button" type="submit" onClick={uploadFile} disabled={enableButton}>Submit</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default FileUploaderModal;
