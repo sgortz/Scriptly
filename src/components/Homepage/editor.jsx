0eb535a3a003b1259e897da90bd1ff4478a2a320
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-05-11 11:11:14 -0400

    Homepage 06 (#50)
    * Initial Commit - Homepage Framework Coming Together
    
    * Working on Text Editor Functionality - Setting to Global States
    
    * Finding Limitation With Built In Text Area Property
    
    * Reverted App.js
    
    * Conflicts
    
    * Conflicts
    
    * Fixed Tabs
    
    * Working On Editable TextArea
    
    * Added Homepage to Landing Page
    
    * Pre-Merge Party
    
    * Pre-Merge
    
    * Merging
    
    * Merging
    
    * Clean Copy
    
    * Package.json
    
    * Results Modal Linked to Homepage
    
    * Pre-Merge
    
    * Merging
    
    * Testing
    
    * Fixing Errors
    
    * Merge
    
    * Merge
    
    * Get Speeches On Page Load
    
    * Switched To Slate Text Editor - Appropriate Speech Loads to Editor
    
    * Pre Merge
    
    * Merging
     On branch Homepage-04
    
    * index.html
    
    * Things are broken, mergin
    
    * Fixed Merging Issues
    
    * More Fun With Index.html
    
    * Morning Commit - Mergin Last Nights Changes
    
    * Added Titling, Form Verification, Overflow/Sizing Control
    
    * Crude Analyzer Working
    
    * Pre-Merge
     Please enter the commit message for your changes. Lines starting
    
    * Made Results Modal Global - Fixed Edited Persistence
    
    * Analyzer Pulling Live Data
    
    * Pre-merge
    
    * Pre-Push - History Linked - ID Passed
    
    * Pre-Merge
    
    * Merging
    
    * Index.Html is the bane of my existence
    
    * Stuff
    
    * Live Email Implemented
    
    * Added In File Modal Uploader To Global State
    
    * Changed Properties On Anger/Fear WordCount/TotalCount Spots
    
    * NPM Install Moment
    
    * Pre-Merge
    
    * Pulling
    
    * Fixed Conflicts
    
    * Conflicts
    
    * Conflicts
    
    Co-authored-by: Trevor Edwards <“trevor.jeffrey.edwards@gmail.com”>

--- src/components/Homepage/editor.jsx
+++ src/components/Homepage/editor.jsx
@@ -21,13 +21,12 @@ const MyEditor = () => {
     setTitle(event.target.value)
   }
 
-
   return (
     <Slate editor={editor} value={initialValue} onChange={(value) => {
       setEdited(value[0].children[0].text)
     }}>
       <form>
-        <input type='text' placeholder='Input Title' onChange={titleListener}></input>
+        <input type='text' placeholder={titleValue} value={titleValue} onChange={titleListener}></input>
       </form>
       <Editable style={{margin: '25px', width: '80vw'}}/>
     </Slate>
