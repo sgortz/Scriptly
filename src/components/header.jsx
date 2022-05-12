c67058058a56e89225ca1dee8b90f3b83324ceb3
Jonathan Ghee <96302073+JonathanGhee@users.noreply.github.com>
2022-05-12 18:15:52 -0500

    added conditionally rendered pages (#78)

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,34 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { GiQuillInk } from 'react-icons/gi';
 import { Link } from 'react-router-dom';
 import './Landing/Landing.css';
 import SignIn from './SignIn.jsx';
+import MeetTeam from './Landing/MeetTeam.jsx';
 
-function Header() {
+function Header({ setPage,showTeam,setShowTeam }) {
+  //const [showTeam, setShowTeam] = useState(false)
   return (
     <div className="header row">
       <div className="main-title col-3">
-        <Link to="/">
+        {/* <Link to="/"> */}
           <GiQuillInk />
           <h1>Scriptly</h1>
-        </Link>
+        {/* </Link> */}
       </div>
       <div className="header-menu col">
         <span className="about-us">
           <a href="https://github.com/SteveOceanProject" target="_blank" rel="noreferrer">About Us</a>
         </span>
-        <Link to="/meet-team" className="team-page">Meet the Team</Link>
+        {/* <Link to="/meet-team" className="team-page">Meet the Team</Link> */}
+        <a className="team-page" onClick={()=>setShowTeam(!showTeam)}>Meet the Team</a>
         <span className="pricing">Pricing</span>
         <span className="about-us">
           <a href="https://github.com/SteveOceanProject" target="_blank" rel="noreferrer">Contact Us</a>
         </span>
         <span className="login-page">
-          <button className="login-button" type="button">LOGIN</button>
-          {/* <SignIn /> */}
+          {/* <button className="login-button" type="button">LOGIN</button> */}
+          <SignIn setPage={setPage} cName={'login-button'} />
         </span>
       </div>
+      {/* {showTeam && <MeetTeam/>} */}
     </div>
   );
 }
