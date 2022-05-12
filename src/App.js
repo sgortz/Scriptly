827fe6f3659ea6a5908d26a4a2108f451d35842b
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-05-11 23:59:05 -0400

    Added React routes for header

--- src/App.js
+++ src/App.js
@@ -2,27 +2,33 @@
 import React, { useState } from 'react';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing/Landing.jsx';
 import Homepage from './components/Homepage/homepage.jsx';
 import Header from './components/header.jsx';
+import MeetTeam from './components/Landing/MeetTeam.jsx';
 import { monitorAuthState } from './auth/Firebase';
 
-const which = () => {
-  return localStorage.email ? 'homepage' : 'landing';
-}
+const which = () => (localStorage.email ? 'homepage' : 'landing');
 
 
 function App() {
   const [page, setPage] = useState(which());
   const [login, setLogin] = useState(false);
-
   const currentPage = page === 'landing' ? <Landing setLogin={setLogin} login={login} setPage={setPage} page={page} /> : <Homepage setPage={setPage} page={page} />;
+
   return (
     <div className="app-container">
-      <Header />
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route path="/meet-team" element={<MeetTeam />} />
+        </Routes>
+      </Router>
       {/* <SignIn/>
         <Homepage /> */}
-      {currentPage}
+      {/* {currentPage} */}
     </div>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,26 +2,33 @@ import React from 'react';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { GiQuillInk } from 'react-icons/gi';
+import { Link } from 'react-router-dom';
 import './Landing/Landing.css';
 import SignIn from './SignIn.jsx';
 
 function Header() {
   return (
     <div className="header row">
-      <h1 className="main-title col-7">
-        <GiQuillInk />
-        Scriptly
-      </h1>
+      <div className="main-title col-3">
+        <Link to="/">
+          <GiQuillInk />
+          <h1>Scriptly</h1>
+        </Link>
+      </div>
       <div className="header-menu col">
         <span className="about-us">
           <a href="https://github.com/SteveOceanProject" target="_blank" rel="noreferrer">About Us</a>
         </span>
-        <span className="team-page">Team</span>
+        <Link to="/meet-team" className="team-page">Meet the Team</Link>
+        <span className="pricing">Pricing</span>
+        <span className="about-us">
+          <a href="https://github.com/SteveOceanProject" target="_blank" rel="noreferrer">Contact Us</a>
+        </span>
         <span className="login-page">
-          <SignIn />
+          <button className="login-button" type="button">LOGIN</button>
+          {/* <SignIn /> */}
         </span>
       </div>
-      {/* <script>login</script> */}
     </div>
   );
 }
