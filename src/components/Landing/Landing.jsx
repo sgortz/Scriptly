f1bfc1e1985df330fa88ae26188b5933f44793a1
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-05-11 23:57:12 -0400

    Landing page styled

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,30 +3,29 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Landing.css';
 import PageImage from '../../assets/PageImage.jsx';
+import JoinBanner from './JoinBanner.jsx';
 
 function Landing(props) {
   const { setLogin, login } = props;
   return (
-    <div className="landing-page">
-      <div className="page-slogan">
-        <h2>
-          Get the tone of any speech you create. Instantly.
-        </h2>
-        <br />
-        <p>
-          Over 220,000 speeches use Scriptly to improve what’s working and fix what isn’t.
-        </p>
-        <button
-          type="submit"
-          className="start-here-button"
-          onClick={() => setLogin(!login)}
-        >
-          Get Started
-        </button>
+    <div>
+
+      <div className="landing-page">
+        <div className="page-slogan">
+          <h2>
+            Get the tone of any speech you create. Instantly.
+          </h2>
+          <br />
+          <p>
+            Save. Analyze. Re-edit. All in one organizational space.
+          </p>
+
+        </div>
+        <div className="page-drawing">
+          <PageImage />
+        </div>
       </div>
-      <div className="page-drawing">
-        <PageImage />
-      </div>
+      <JoinBanner login={login} setLogin={setLogin} />
     </div>
   );
 }
