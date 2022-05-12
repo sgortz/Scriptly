e9ebab424e69e1e5c65f937671ba881c3ad2cd09
Amina <amina.thiam.pro@gmail.com>
2022-05-12 23:17:50 +0200

    Initialize card animation and layout

--- src/results/Result2.jsx
+++ src/results/Result2.jsx
@@ -5,46 +5,17 @@ import {
   BsFillEmojiAngryFill,
   BsFillEmojiLaughingFill,
   BsFillEmojiSmileFill,
-  BsFillEmojiFrownFill ,
-  BsFillEmojiWinkFill
+  BsFillEmojiFrownFill,
+  BsFillEmojiWinkFill,
+  BsFillHandThumbsUpFill,
 } from 'react-icons/bs';
 
-export default function Result2({ changePage, strongEmotions, weakEmotions }) {
-  const allEmojis = {
-    anger:
-      <BsFillEmojiAngryFill className="emotion-icon"/> ,
-    joy:
-      <BsFillEmojiLaughingFill className="emotion-icon"/> ,
-    trust:
-      <BsFillEmojiWinkFill className="emotion-icon"/> ,
-    negative:
-      <BsFillEmojiFrownFill className="emotion-icon"/> ,
-    positive:
-    <BsFillEmojiSmileFill className="emotion-icon"/>
-  };
+export default function Result2({ changePage, strongEmotions, emotions }) {
+  // const [showReco, setShowReco] = useState(false);
 
-  const mainEmotions = strongEmotions.map(emotion =>
-    // <Col >
-      <div className="card-wrapper biggest-card" id={emotion} key={Math.random() * 1000}>
-        <div className="card-content-container">
-          {allEmojis[emotion]}
-          <h3>{emotion === 'anger' ? 'fear' : emotion}</h3>
-        </div>
-      </div>
-    // </Col>
-  )
-
-  const otherEmotions = weakEmotions.map(emotion =>
-    // <Col >
-      <div className="card-wrapper" id={emotion} key={Math.random() * 1000}>
-        <div className="card-content-container">
-          {allEmojis[emotion]}
-          <h3>{emotion === 'anger' ? 'fear' : emotion}</h3>
-        </div>
-      </div>
-    // </Col>
-  )
-
+  const handleHover = () => {
+    setShowReco(true);
+  }
 
   return (
     <>
@@ -52,12 +23,67 @@ export default function Result2({ changePage, strongEmotions, weakEmotions }) {
         Tone analysis 2/4
       </Modal.Header>
       <Modal.Body>
-        {/* <Container> */}
-          {/* <Row> */}
-            {mainEmotions}
-            {otherEmotions}
-          {/* </Row> */}
-        {/* </Container> */}
+        <div className="all-cards-container">
+
+          {/* Fear  */}
+          <div className={ strongEmotions.indexOf('anger') !== - 1 ? 'card-wrapper main-tone' : 'card-wrapper'} id="anger" key={Math.random() * 1000}>
+            <div className="card-content-container">
+              <BsFillEmojiAngryFill className="emotion-icon"/>
+              <h3>Fearful</h3>
+            </div>
+          </div>
+
+          {/* Negative */}
+          <div className={ strongEmotions.indexOf('negative') !== - 1 ? 'card-wrapper main-tone' : 'card-wrapper'} id="negative" key={Math.random() * 1000}>
+            <div className="card-content-container">
+              <BsFillEmojiFrownFill className="emotion-icon"/>
+              <h3>Negative</h3>
+            </div>
+          </div>
+
+{/* { strongEmotions.indexOf('positive') !== - 1 && strongEmotions.indexOf('trust') === - 1 ? */}
+
+
+          {/* Positive */}
+          <div className={ strongEmotions.indexOf('positive') !== - 1 ? 'card-wrapper main-tone' : 'card-wrapper'} id="positive" key={Math.random() * 1000}>
+            <div className="full-card">
+              <div className="front-card">
+                <BsFillEmojiSmileFill className="emotion-icon"/>
+                <h3>Positive</h3>
+                  <p className="score"><BsFillHandThumbsUpFill className="good-score"/>{emotions.positive}
+                  </p>
+                  <p className="result-text">
+                    Great job, your speech mainly conveys a positive tone!
+                  </p>
+              </div>
+              <div className="back-card">
+                <BsFillEmojiSmileFill className="emotion-icon"/>
+                <h3>Positive</h3>
+                <p className="result-text">
+                  <span>💡 Tips:</span> if you aim at convincing and reuniting your audience, consider slightly improving your Trustful tone score.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* Joy */}
+          <div className={ strongEmotions.indexOf('joy') !== - 1 ? 'card-wrapper main-tone' : 'card-wrapper'} id="joy" key={Math.random() * 1000}>
+            <div className="card-content-container">
+              <BsFillEmojiLaughingFill className="emotion-icon"/>
+              <h3>Joyful</h3>
+            </div>
+          </div>
+
+          {/* Trust */}
+          <div className={ strongEmotions.indexOf('trust') !== - 1 ? 'card-wrapper main-tone' : 'card-wrapper'} id="trust" key={Math.random() * 1000}>
+            <div className="card-content-container">
+              <BsFillEmojiWinkFill className="emotion-icon"/>
+              <h3>Trustful</h3>
+            </div>
+          </div>
+          {/* {mainEmotions}
+          {otherEmotions} */}
+        </div>
       </Modal.Body>
       <Modal.Footer className="results-footer">
         <p>{`Your speech mainly convey a ${strongEmotions[0]} tone.`}</p>
--- src/results/Results.jsx
+++ src/results/Results.jsx
@@ -78,7 +78,7 @@ export default function Results(props) {
           <Result2
             changePage={changeResultPage}
             strongEmotions={strongestEmotions}
-            weakEmotions={weakestEmotions}
+            emotions={displayedAnalysis}
           /> :
           resultPage === 3 ?
           <Result3
