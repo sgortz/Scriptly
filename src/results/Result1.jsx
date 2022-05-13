ca5394fd562e2a00ba224b9e83c05813ffe50416
Amina Thiam <amina.thiam.pro@gmail.com>
2022-05-13 21:14:07 +0200

    Fix bug when no tonality or a tonality of 0 (#91)
    * Initialize results modal view with Result1 chart and Result3 chart
    
    * Initialize all results pages with dummy data
    
    * Fix bug modal
    
    * Add styling
    
    * Add basic styling to Cards
    
    * Add basic styling
    
    * Update results pages with actual data, except result2
    
    * Fix bug with fear analysis and totalCount
    
    * Fix Result2 bugs and slightly upadate paragraph analysis
    
    * Update anger to fear on Result2
    
    * Fix bug with results Page not reseting to 1
    
    * Initialize speech post to DB on submit on result page 4
    
    * Handle analyze button and submit on result4 to save in DB
    
    * Fix bug neutral tone on result page 3
    
    * Fix merge conflicts
    
    * Fix bug speeches display
    
    * Initialize styling to cards
    
    * Initialize card animation and layout
    
    * Style Result1 modal page
    
    * Style majority of Result4
    
    * Add flip card effect on result2
    
    * Add flip card effect to all tones on result2
    
    * Fix numbers bug on result2 cards
    
    * Change cards text according to each tone results
    
    * Change thumbs icon according to tone results
    
    * Layout partially submit button
    
    * Fix bug when no tonality or a tonality of 0 for some

--- src/results/Result1.jsx
+++ src/results/Result1.jsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Chart as ChartJS, Legend, Tooltip, Title, ArcElement } from 'chart.js';
+import { useRecoilState } from 'recoil';
 import { Doughnut } from 'react-chartjs-2';
 import { Modal } from 'react-bootstrap';
 import { BsFillCaretLeftFill, BsFillCaretRightFill } from 'react-icons/bs';
 import './styles.css';
+import axios from 'axios';
+import { editedSpeechText, updateTitle, currentSpeechId, editBoolean, pageView, allSpeeches, resultsModal, formattedSpeech } from './../atoms.jsx';
 
-export default function Result1({ wordsCount, emotionCount, changePage, neutralCount }) {
+export default function Result1({ wordsCount, emotionCount, changePage, neutralCount, emotions }) {
+  const [noStrongTonality, setNoStrongTonality] = useState(false);
+
+  const [editedValue, setEdited] = useRecoilState(editedSpeechText);
+  const [titleValue, setTitle] = useRecoilState(updateTitle);
+  const [currentId, setCurrentId] = useRecoilState(currentSpeechId);
+  const [editBooleanValue, setEditBoolean] = useRecoilState(editBoolean);
+  const [pageValue, setPage] = useRecoilState(pageView);
+  const [speechValue, setSpeechValue] = useRecoilState(allSpeeches);
+  const [showResults, setShowResults] = useRecoilState(resultsModal);
+  const [formattedValue, setFormatted] = useRecoilState(formattedSpeech);
+
+  useEffect(() => {
+    if (!emotionCount) {
+      setNoStrongTonality(true);
+    } else {
+      setNoStrongTonality(false);
+    }
+  }, [emotionCount]);
+
+  const handleSubmit = () => {
+    let url = '';
+    setShowResults(false);
+
+    if (!editBooleanValue) {
+      url = `/speech/`
+    } else if (editBooleanValue) {
+      url = `/speech/${currentId}`
+    } else {
+      return 'Invalid submit';
+    }
+
+    axios.post(url, {
+      body: `${formattedValue}`,
+      title: `${titleValue}`,
+      name: 'Trevor Edwards',
+      email: `${localStorage.email}`,
+      totalCount: emotions.totalCount,
+      positive: emotions.positive,
+      negative: emotions.negative,
+      trust: emotions.trust,
+      anger: emotions.anger,
+      joy: emotions.joy,
+    })
+    .then((response) => {
+      axios.get(`/history/${localStorage.email}`)
+      .then((res) => {
+        setSpeechValue(res.data);
+        setPage('speech');
+        setEditBoolean(false);
+        setEdited('      ');
+        setTitle('');
+      })
+      .catch((error) => {
+        console.log('error')
+      })
+    })
+    .catch((error) => {
+      console.error(error, 'this is a post error')
+    })
+  }
+
   ChartJS.register(Legend, Tooltip, Title, ArcElement);
 
   const data = {
@@ -60,7 +124,11 @@ export default function Result1({ wordsCount, emotionCount, changePage, neutralC
 
       <p>{`Out of the ${wordsCount} words of your speech, ${emotionCount} have a strong tonality`}</p>
 
-      <div className="btn btn-primary results-btn" data-toggle="modal" data-target="#exampleModal" onClick={() => changePage(1)}><BsFillCaretRightFill/></div>
+      { noStrongTonality ?
+        <button type="button" className="btn btn-primary submit-speech" data-toggle="modal" data-target="#exampleModal" onClick={handleSubmit}>Submit</button>
+        :
+        <div className="btn btn-primary results-btn" data-toggle="modal" data-target="#exampleModal" onClick={() => changePage(1)}><BsFillCaretRightFill/></div>
+      }
     </Modal.Footer>
   </>
   )
